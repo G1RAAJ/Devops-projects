2ed0cc8f0667b332df18a083d9c3077c973127db
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-20 16:36:48 +0530

    Update eks.tf

--- Nginx_eks/Terraform/eks.tf
+++ Nginx_eks/Terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version         = "20.0.0"
 
   cluster_name    = "eks"
-  cluster_version = "1.29"
+  cluster_version = "1.28"
 
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
