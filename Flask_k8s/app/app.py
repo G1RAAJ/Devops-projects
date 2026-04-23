2ccf1a2d42e9a239c6465b9420d284f0990eff13
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-23 22:00:31 +0530

    Update app.py

--- Flask_k8s/app/app.py
+++ Flask_k8s/app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Kubernetes CI/CD Pipeline!"
+    return "Your web app is successfully deployed!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
