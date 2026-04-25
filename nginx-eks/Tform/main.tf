c42b07064bb0ba22b2e9654babfdf2bee1ef87c7
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-25 18:30:57 +0530

    Update main.tf

--- nginx-eks/Tform/main.tf
+++ nginx-eks/Tform/main.tf
@@ -1,16 +1,38 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
+############################
+# VPC (Auto create)
+############################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+############################
+# EKS Cluster
+############################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "20.37.2"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_name    = "myeks"
+  cluster_version = "1.29"
 
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnet_ids
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
@@ -21,4 +43,3 @@ module "eks" {
     }
   }
 }
-
