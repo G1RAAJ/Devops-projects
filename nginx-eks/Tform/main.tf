bdd250863a574cd5dd4dab350719bcab9952f6fc
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-25 20:32:16 +0530

    Update main.tf

--- nginx-eks/Tform/main.tf
+++ nginx-eks/Tform/main.tf
@@ -1,38 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-############################
-# VPC (Auto create)
-############################
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.0"
-
-  name = "eks-vpc"
-
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
-############################
-# EKS Cluster
-############################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.37.2"
 
   cluster_name    = "myeks"
-  cluster_version = "1.29"
+  cluster_version = "1.30"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
+  create_cloudwatch_log_group = false   # ✔ HERE
 
   eks_managed_node_groups = {
     default = {
