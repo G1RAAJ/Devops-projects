0ef879388e609993e20860c63a867aa6f907a877
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-29 14:03:50 +0530

    Add files via upload

---
+++ nginx-myeks/Tform/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.37.2"
+
+  cluster_name    = "myeks-2"
+  cluster_version = "1.30"
+
+  vpc_id = "vpc-09a23ce107252dc4b"
+
+  subnet_ids = [
+    "subnet-00de52fb1394854b6",
+    "subnet-02df3f74c0714b806"
+  ]
+
+  cluster_endpoint_public_access = true
+
+  create_cloudwatch_log_group = false
+  create_kms_key              = false
+  cluster_encryption_config   = []
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
+    }
+  }
+}
