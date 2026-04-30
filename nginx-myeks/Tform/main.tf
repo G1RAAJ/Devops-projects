d6729889793f0bf53c9b3ac5a9c7855d90a88799
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-30 20:30:18 +0530

    Update main.tf

--- nginx-myeks/Tform/main.tf
+++ nginx-myeks/Tform/main.tf
@@ -17,7 +17,7 @@ module "eks" {
   ]
 
   cluster_endpoint_public_access       = true
-  cluster_endpoint_private_access      = true
+  cluster_endpoint_private_access      = false
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   create_cloudwatch_log_group = false
