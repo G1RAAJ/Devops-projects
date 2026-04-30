52594d1748ed262ed17468a8c750c7b0d1ce70d1
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-30 21:17:28 +0530

    Update main.tf

--- nginx-myeks/Tform/main.tf
+++ nginx-myeks/Tform/main.tf
@@ -1,3 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+########################################
+# EKS CLUSTER
+########################################
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.37.2"
@@ -5,10 +13,48 @@ module "eks" {
   cluster_name    = "myeks-2"
   cluster_version = "1.30"
 
-  vpc_id     = "vpc-09a23ce107252dc4b"
-  subnet_ids = ["subnet-04a75877b20cf9bf4", "subnet-079b66fb0be7efc65"]
+  vpc_id = "vpc-09a23ce107252dc4b"
 
-  # ✅ ADD HERE 👇
+  subnet_ids = [
+    "subnet-04a75877b20cf9bf4",
+    "subnet-079b66fb0be7efc65"
+  ]
+
+  ########################################
+  # 🔥 NETWORK FIX (VERY IMPORTANT)
+  ########################################
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_private_access      = false
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+  ########################################
+  # NODE GROUP
+  ########################################
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Project = "nginx-eks"
+  }
+}
+
+########################################
+# AWS AUTH (FIX FOR JENKINS ACCESS)
+########################################
+
+module "aws_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "20.37.2"
+
   manage_aws_auth_configmap = true
 
   aws_auth_roles = [
@@ -18,18 +64,4 @@ module "eks" {
       groups   = ["system:masters"]
     }
   ]
-
-  # networking fix (important)
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_private_access      = false
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
-
-  eks_managed_node_groups = {
-    default = {
-      instance_types = ["t3.small"]
-      desired_size   = 2
-      min_size       = 1
-      max_size       = 3
-    }
-  }
 }
