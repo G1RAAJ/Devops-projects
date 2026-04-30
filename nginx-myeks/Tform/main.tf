2f3df7352f1d73b486afa4b3a9d441910c5108eb
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-30 15:32:48 +0530

    Update main.tf

--- nginx-myeks/Tform/main.tf
+++ nginx-myeks/Tform/main.tf
@@ -12,8 +12,8 @@ module "eks" {
   vpc_id = "vpc-09a23ce107252dc4b"
 
   subnet_ids = [
-    "subnet-00de52fb1394854b6",
-    "subnet-02df3f74c0714b806"
+    "subnet-028322a73eba78e51",
+    "subnet-04fa50ba482c9953e"
   ]
 
   cluster_endpoint_public_access = true
