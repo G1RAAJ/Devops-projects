02e3f1a12718ca8683df7c6cc00c6b92e2d56398
G1RAAJ <jeevanraju.ch@gmail.com>
2026-05-06 15:01:35 +0530

    Update main.tf

--- webapp-project/terraform/main.tf
+++ webapp-project/terraform/main.tf
@@ -23,16 +23,29 @@ module "eks" {
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
 
+  # Enable public API access
   cluster_endpoint_public_access = true
 
+  # Optional but recommended
+  enable_irsa = true
+
   eks_managed_node_groups = {
+
     default = {
+
       desired_size = 2
       max_size     = 3
       min_size     = 1
 
       instance_types = ["t3.small"]
-      ami_type       = "AL2_x86_64"
+
+      ami_type = "AL2_x86_64"
+
+      capacity_type = "ON_DEMAND"
     }
+  }
+
+  tags = {
+    Project = "webapp-project"
   }
 }
