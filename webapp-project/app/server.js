4a01a6760bdae53d3dc188d04ab5b00f5a58fe86
G1RAAJ <jeevanraju.ch@gmail.com>
2026-05-07 16:39:43 +0530

    Update server.js

--- webapp-project/app/server.js
+++ webapp-project/app/server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
 
-const app = express();
+const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express()
 
-app.get('/health', (req, res) => {
-  res.json({
-    status: 'UP'
-  });
-});
+const PORT = 3000
 
-app.listen(3000, () => {
-  console.log('Application running on port 3000');
-});
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+})
